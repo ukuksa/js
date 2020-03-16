e217414991a78af0b3984a199865f3b3557539ad
Vlad <donivinik@gmail.com>
2020-03-16 18:13:32 +0100

    first commuit

---
+++ udemy kurs/script.js
@@ -0,0 +1,49 @@
+
+
+//lesson 1
+// var leftBorderWidth = 1;
+// let second = 2;
+// const pi = 3.14;
+// console.log(leftBorderWidth);
+// console.log(second);
+
+
+var number = 5;
+var string = "Hello!";
+var sym = Symbol();
+var boolean = true;
+null;
+undefined;
+var obj = {};
+
+let something;
+console.log(something);
+let person = {
+    name: "John",
+    surname: "Smith",
+    age: 25,
+    isMarried: false
+};
+console.log(person["name"]);
+
+
+let arr = ['plum.png','orange.jpg','apple.phg'];
+
+console.log(arr[0]);
+
+//alert("Hello world!");
+// let answer = confirm("Do you want choose this?");
+// console.log(answer);
+
+// let answer = +prompt("Are you have 18+?", "Sure");
+
+// console.log(answer);
+
+// console.log("arr" + " - object");
+
+let incr = 10,
+    decr = 10;
+
+incr++;
+decr--;
+console.log(incr, decr);
